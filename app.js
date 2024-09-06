27fe02bd288d93a7e608e49a0583ba90f9a1b567
Deividas Kozlovas <kozlovas.deividas@gmail.com>
2024-09-06 18:41:08 +0300

    created initial home page UI with css styling

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const app = express();
 
 app.use(express.json());
@@ -10,8 +11,12 @@ mongoose.connect('mongodb://localhost:27017/mydatabase', {
 }). then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('MongoDB connection error:', err));
 
+//To serve files in "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve static files from the "public" directory
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
